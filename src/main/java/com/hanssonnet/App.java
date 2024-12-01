7698bcc29cb8a0fae97eb0ae416f13dbb1f2771a
Tobias Hansson <tobiashansson93@gmail.com>
2024-12-01 21:15:23 +0100

    add java base skeleton

---
+++ src/main/java/com/hanssonnet/App.java
@@ -0,0 +1,13 @@
+package com.hanssonnet;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
